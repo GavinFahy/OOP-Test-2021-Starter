4fd8a4451b6450656ce9109a47eafda7a26f6888
Gavin Fahy <gavinf37@gmail.com>
2021-04-21 16:33:38 +0100

    Created ArrayList

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -7,6 +7,8 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
+	ArrayList<Note> notes = new ArrayList<Note>();
+	
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
